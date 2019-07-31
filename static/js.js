e692445c1591fcdb59f84c81486a8e26050ca3f3
myname21-meet <myname21@meet.mit.edu>
2019-07-31 04:27:14 -0400

    Just Testing Horuku will get shit back later

--- static/js.js
+++
@@ -1,6 +0,0 @@
-function changetheme() {
-	var x = document.getElementsByTagName("BODY")
-	x.style.backgroundColor
-	document.body.style.backgroundColor = "#1a1a1a";
-	// document.body.style.color = "#e5e5e5";
-}
